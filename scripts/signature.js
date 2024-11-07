e2ebc3fc38bf0449493fa9f21add7790badf183a
Benjamin <fireblazegaming21@gmail.com>
2024-11-07 22:45:13 +0100

    Hopefully Final Final update
    I am beyond done with dis

--- scripts/signature.js
+++ scripts/signature.js
@@ -4,7 +4,10 @@ const clear = document.querySelector('.delete')
 const ctx = canvas.getContext('2d');
 const body = document.getElementsByTagName('body');
 
+
+
 let writingMode = false;
+const coordinates = [];
 
 const clearPad = () => {
     ctx.clearRect(0,0, canvas.width, canvas.height);
@@ -13,6 +16,7 @@ const clearPad = () => {
 clear.addEventListener('click', (event) => {
     event.preventDefault();
     clearPad();
+    coordinates.length = 0;
 })
 
 ctx.lineWidth = 3;
@@ -28,22 +32,30 @@ const getTargetPosition = (event) => {
 
 //fix da ko je darkmode piše z belo
 
+const switcher = document.querySelector(".themeSwitch").addEventListener("click", function(){
+    if(body[0].classList.contains("darkMode")){
+        ctx.strokeStyle = "black";
+        redraw();
+    }
+    else{
+        ctx.strokeStyle = "white";
+        redraw();
+    }
+    });
+
+
 const handlePointerMove = (event) => {
     if(!writingMode) return
 
     const [posX, posY] = getTargetPosition(event);
     ctx.lineTo(posX,posY);
     ctx.stroke();
-    if(body.classList.contains(".darkmode"))
-        ctx.strokeStyle = "white";
-    else
-        ctx.strokeStyle = "black";
-
-
+    coordinates.push({ x: posX, y: posY });
 }
 
 const handlePointerUp = () => {
     writingMode = false;
+    coordinates.push({ x: null, y: null });
 }
 
 const handlePointerDown = (event) => {
@@ -52,11 +64,27 @@ const handlePointerDown = (event) => {
 
     const [posX, posY] = getTargetPosition(event);
     ctx.moveTo(posX,posY);
+    coordinates.push({ x: posX, y: posY });
 }
 
 const handlePointerOut = () => {
     writingMode = false;
 }
+
+const redraw = () => {
+    clearPad();
+    ctx.beginPath();
+    for (let i = 0; i < coordinates.length; i++) {
+        const point = coordinates[i];
+        if (point.x === null && point.y === null) {
+            ctx.beginPath(); // Start a new path for each separate stroke
+        } else {
+            ctx.lineTo(point.x, point.y);
+            ctx.stroke();
+        }
+    }
+}
+
 
 canvas.addEventListener('pointerdown', handlePointerDown, {passive: true});
 canvas.addEventListener('pointerup', handlePointerUp, {passive: true});
