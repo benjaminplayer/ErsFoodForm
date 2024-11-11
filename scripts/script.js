25da77761be61f7cba50db0c0d7eeef23497f837
Benjamin <fireblazegaming21@gmail.com>
2024-11-11 20:44:50 +0100

    Smol updates

--- scripts/script.js
+++ scripts/script.js
@@ -66,12 +66,18 @@ document.getElementById('emsoin').addEventListener("input", function (e) {
     e.target.value = value.slice(0, 13); // ohrani samo prvih 13 stevil
 });
 
+document.querySelectorAll('input[type="text"]').forEach(input => { //vsakemu elementu tipa input text doda event listener, ki kliče funkcijo z eventom e
+    input.addEventListener('input', (e) => {
+        e.target.value = e.target.value.slice(0,30); //omeji na max 30 characters input na vseh poljih
+    });
+});
+
 let masks = document.querySelectorAll(".onlyLetters")
 masks.forEach(mask => mask.addEventListener("input", function (e) {
     const value1 = e.target.value.replace(/[^a-zA-Z]/g, ''); //izbriše use številke 
     e.target.value = value1;
 }));
-//prepreči da popup da submit
+//prepreči da popup izvede submit
 
 const preventPopup = document.getElementsByClassName('mainForm');
 
@@ -86,64 +92,97 @@ document
 function validate(event) {
     event.preventDefault(); // prepreči submit
     const requiredFields = document.querySelectorAll("#mainFormus [required]");
-    let allFiled = true;
+    let arr = ["ime vlagatelja", "priimkek vlagatelja","naslov vlagatelja","vlogo vlagatelja"," ime dijaka", "priimek dijaka","emso dijaka","naslov dijaka"];
 
-    requiredFields.forEach((field) => { //preveri ali so polja povna
-        if (!field.value.trim()) { //.trim() odstrani use extra spaces
-            allFiled = false;
+    // gre z loopom skozi vsa obvezna polja, ter iz zgornje tabele vstavi ustrezno obliko zapisa v alert
+    for(let i = 0; i < arr.length; i++){
+        console.log(requiredFields[i]);
+        if(i === 3 && !cehckRadio()){
+                Swal.fire({
+                    title: 'Napaka!',
+                    text: 'Prosimo izberite '+ arr[i],
+                    icon: 'error',
+                    iconColor: '#EF5041',
+                    confirmButtonText: 'OK',
+                    confirmButtonColor: '#EF5041'
+                });
+                return;
+        }else if(!requiredFields[i].value.trim()){
+            Swal.fire({
+                title: 'Napaka!',
+                text: 'Prosimo vnesite ' + arr[i],
+                icon: 'error',
+                iconColor: '#EF5041',
+                confirmButtonText: 'OK',
+                confirmButtonColor: '#EF5041'
+            });
+            return;
         }
-    });
+    }
 
+    if(!checkSelections())
+        return;
+
+    if(!document.getElementById("prijDate").value){ //če datum nima vrednosti je vrednost "" -> "" pomeni false
+        Swal.fire({
+            title: 'Napaka!',
+            text: 'Prosimo vnesite datum veljavnosti',
+            icon: 'error',
+            iconColor: '#EF5041',
+            confirmButtonColor: '#EF5041',
+            confirmButtonText: 'OK'
+        });
+        return;
+    } 
     if (isCanvasBlank()){
-        allFiled = false;
             Swal.fire({
                 title: 'Napaka!',
                 text: 'Podpis je obvezen vnos!',
                 icon: 'error',
+                iconColor: '#EF5041',
+                confirmButtonColor: '#EF5041',
                 confirmButtonText: 'OK'
             });
             return;
     }
         
     if (!document.getElementById("izjavaCheck").checked) {
-        allFiled = false;
+        Swal.fire({
+            title: 'Napaka!',
+            text: 'Z izjavo se morate strinjati!',
+            icon: 'error',
+            iconColor: '#EF5041',
+            confirmButtonColor: '#EF5041',
+            confirmButtonText: 'OK'
+        });
+        return;
     }
-
-    if(!checkSelections())
-        allFiled = false;
 
     if(!emsoLen()){
         Swal.fire({
             title: 'Napaka!',
             text: 'Emso mora vsebovati 11 stevil!',
             icon: 'error',
+            iconColor: '#EF5041',
+            confirmButtonColor: '#EF5041',
             confirmButtonText: 'OK'
         });
         return;
     }
         
-
-    if (!allFiled) {
-        Swal.fire({
-            title: 'Napaka!',
-            text: 'Prosimo, izpolnite vsa polja!',
-            icon: 'error',
-            confirmButtonText: 'OK'
-        })
-        return;
-    }
-
-    else {
-        Swal.fire({
-            title: 'Prijava je bila uspešno oddana!',
-            icon: 'success'
-        }).then((result) => {
-            if (result.isConfirmed) { //pošlje podatke po tem ko user klikne OK
-                event.target.submit();
-                clearPad();
-            }
-        });
-    }
+    Swal.fire({
+        title: 'Prijava je bila uspešno oddana!',
+        icon: 'success',
+        iconColor: '#49BA86',
+        confirmButtonColor: '#49BA86',
+        confirmButtonText: 'Zaključi!'
+    }).then((result) => {
+        if (result.isConfirmed) { //pošlje podatke po tem ko user klikne OK
+            event.target.submit();
+            clearPad();
+        }
+    });
+    
 }
 
 // preveri ali so usi barvni kanali v vsakem pixlu blank (0)
@@ -160,7 +199,6 @@ function emsoLen() {
         return false;
     return true;    
 }
-
 //odstrani past dates
 
 function disablePastDates() {
@@ -178,15 +216,40 @@ function checkSelections() {
     let select2 = document.getElementById("oddelekSelect");
     if(select.value == 0){
         console.log("doc1 false")
+        Swal.fire({
+            title: 'Napaka!',
+            text: 'Prosimo izberite šolo!',
+            icon: 'error',
+            confirmButtonText: 'OK',
+            iconColor: '#EF5041',
+            confirmButtonColor: '#EF5041'
+        });
         return false;
     }
         
     if(select2.value == "null"){
         console.log("doc2 false")
+        Swal.fire({
+            title: 'Napaka!',
+            text: 'Prosimo izberite oddelek',
+            icon: 'error',
+            confirmButtonText: 'OK',
+            iconColor: '#EF5041',
+            confirmButtonColor: '#EF5041'
+        });
         console.log(select2.value);
         return false;
     }
     return true;
+}
+
+function cehckRadio(){
+   let radios = document.getElementsByClassName('radios');
+   let empty = 0; 
+   for (let i = 0; i < radios.length; i++)
+        if(radios[i].checked)
+            return true;
+    return false;
 }
 
 document.getElementById("signField").addEventListener("touchstart", function(){
@@ -233,7 +296,7 @@ themeSwitch.addEventListener("click", () => {
 
 
 /*
-    Daj sweetalerts na use
-    change colors od gumbu
-    omeji lengths od inputs
+    Daj sweetalerts na use - Done
+    change colors od gumbu 
+    omeji lengths od inputs -Done
 */
